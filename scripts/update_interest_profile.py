ea15644a93d90e46811d5c035a2f4a32a4258d47
knottedTail <philip94@snu.ac.kr>
2026-04-15 16:04:15 +0900

    Add checked arXiv review for 2026-04-15

--- scripts/update_interest_profile.py
+++ scripts/update_interest_profile.py
@@ -13,7 +13,7 @@ from typing import Any
 
 REVIEW_ITEM_PATTERN = re.compile(
     r"^### \[(?P<checked>[ xX])\] (?P<title>.+?)\n\n"
-    r"\*\*Authors:\*\* (?P<authors>.*?) {2}\n"
+    r"(?:\*\*Authors:\*\* (?P<authors>.*?) {2}\n)?"
     r"\*\*Categories:\*\* (?P<categories>.*?) {2}\n"
     r"\*\*Keywords:\*\* (?P<keywords>.*?)\n\n"
     r"\*\*Abstract\*\* {2}\n"
